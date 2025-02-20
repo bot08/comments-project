0f0fd43ecba0bef6803a843fb63c72ff8ad8b36a
Oleg <scherbaoleg4@gmail.com>
2025-02-20 16:49:01 +0200

    Update Blur.vue

--- components/Base/Blur.vue
+++ components/Base/Blur.vue
@@ -1,12 +1,5 @@
 <template>
-  <transition
-    enter-active-class="transition ease-out duration-250"
-    enter-from-class="opacity-0"
-    leave-active-class="transition ease-out duration-250"
-    leave-to-class="opacity-0"
-  >
-    <div v-if="blurStore.isBlur" class="fixed z-10 backdrop-blur inset-0 bg-white/25 dark:bg-black/25"></div>
-  </transition>
+  <div></div>
 </template>
 
 
